3c636b38e220aee97d57c8622dad9f5dd64b8888
gandar <davidezenobigarcia@gmail.com>
2025-01-06 00:08:36 +0100

    supportable object holder component creation and basic script

---
+++ scenes/components/supportable_object_holder/supportable_object_holder.gd
@@ -0,0 +1,24 @@
+extends Node
+
+## Este script habilita la mecanica de apoyar objetos.
+##
+## IMPORTANTE
+## Es requisito que la escena donde se aplique este componente,
+## tenga un nodo referencia donde se va a apoyar el objeto.
+## IMPORTANTE
+
+var current_supportable_object: Node = null;
+
+func support(supportable_object: Node):
+	if current_supportable_object:
+		print("Ya existe un objeto. No se puede apoyar");
+		return;
+	
+	current_supportable_object = supportable_object;
+	return;
+
+func unsupport():
+	
+	current_supportable_object = null;
+	return;
+	
